951f9755f4b329e4171ce7c9eea3af6c7f921ac2
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2014-04-02 02:22:25 +0100

    added server node.js socket.io

---
+++ laser-server/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var io = require('socket.io').listen(12345);
+var routes = require('./routes');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.sockets.on('connection', function(socket) {
+  socket.on('echo', function(data) {
+     socket.broadcast.emit('position',data);
+  });
+});
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ laser-server/public/javascripts/pointer.js
@@ -0,0 +1,44 @@
+var pointer = angular.module('pointer', []);
+
+pointer.factory('socket', function ($rootScope) {
+  var socket = io.connect(window.location.protocol + "//" + window.location.hostname  + ":12345");
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
+
+pointer.controller('ball', ['$scope','socket', function($scope, socket) {
+    $scope.height =$(window).height()/2;
+    $scope.width = $(window).width()/2;
+    $scope.size = 20;
+    socket.on('position', function (data) {
+      $scope.coor = data;
+
+      var h_aux = $scope.height+(-parseFloat(data.y));
+      var w_aux = $scope.width+(-parseFloat(data.x));
+
+      if( h_aux < $(window).height() && h_aux > 0)
+        $scope.height+=-parseFloat(data.y);
+      if( w_aux < $(window).width() && w_aux > 0)
+        $scope.width+=-parseFloat(data.x);
+
+      //$scope.size += parseFloat(data.z);
+    });
+}]);
